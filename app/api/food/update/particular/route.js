29cb6765a656e6db3b74192b7bd6a5d3f62d66d3
Niket Tiwari <96378627+Tiwari-Niket@users.noreply.github.com>
2023-07-12 17:57:11 +0530

    Create route.js

---
+++ app/api/food/update/particular/route.js
@@ -0,0 +1,34 @@
+import Food from "@models/food";
+import { connectToDB } from "@utils/database";
+
+// PATCH (update)
+export const PATCH = async (request) => {
+    const { department_name } = await request.json();
+    console.log(department_name);
+    try {
+        await connectToDB();
+        const existingFood = await Food.find({});
+        // console.log(existingFood);
+        if (!existingFood) return new Response("Food not found", { status: 404 });
+
+        for (let index = 0; index < existingFood?.length; index++) {
+            if (department_name === "dept1") {
+                existingFood[index].dept1_count = existingFood[index].dept1_count -1;
+                await existingFood[index].save();
+            } else if (department_name === "dept2") {
+                existingFood[index].dept2_count = existingFood[index].dept2_count -1;
+                await existingFood[index].save();
+            } else if (department_name === "dept3") {
+                existingFood[index].dept3_count = existingFood[index].dept3_count - 1;
+                await existingFood[index].save();
+            } else {
+                existingFood[index].dept4_count = existingFood[index].dept4_count - 1;
+                await existingFood[index].save();
+            }
+        }
+
+        return new Response("Updated", { status: 200 });
+    } catch (error) {
+        return new Response("Failed to update a count", { status: 500 });
+    }
+};
