d2b77478acdab2a5a3617104daadef71be0df335
Niket Tiwari <96378627+Tiwari-Niket@users.noreply.github.com>
2023-07-19 17:45:57 +0530

    Update route.js

--- app/api/food/update/particular/route.js
+++ app/api/food/update/particular/route.js
@@ -3,27 +3,26 @@ import { connectToDB } from "@utils/database";
 
 // PATCH (update)
 export const PATCH = async (request) => {
-    const { department_name } = await request.json();
-    console.log(department_name);
+    const { department_name, ordered } = await request.json();
     try {
         await connectToDB();
-        const existingFood = await Food.find({});
-        // console.log(existingFood);
-        if (!existingFood) return new Response("Food not found", { status: 404 });
 
-        for (let index = 0; index < existingFood?.length; index++) {
+        for (let index = 0; index < ordered.length; index++) {
+            const existingFood = await Food.findOne({food_name: ordered[index]});
+
+            if (!existingFood) return new Response("Food not found", { status: 404 });
             if (department_name === "dept1") {
-                existingFood[index].dept1_count = existingFood[index].dept1_count -1;
-                await existingFood[index].save();
+                existingFood.dept1_count = existingFood.dept1_count - 1;
+                await existingFood.save();
             } else if (department_name === "dept2") {
-                existingFood[index].dept2_count = existingFood[index].dept2_count -1;
-                await existingFood[index].save();
+                existingFood.dept2_count = existingFood.dept2_count - 1;
+                await existingFood.save();
             } else if (department_name === "dept3") {
-                existingFood[index].dept3_count = existingFood[index].dept3_count - 1;
-                await existingFood[index].save();
+                existingFood.dept3_count = existingFood.dept3_count - 1;
+                await existingFood.save();
             } else {
-                existingFood[index].dept4_count = existingFood[index].dept4_count - 1;
-                await existingFood[index].save();
+                existingFood.dept4_count = existingFood.dept4_count - 1;
+                await existingFood.save();
             }
         }
 
